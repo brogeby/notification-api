d1ab14af6d8432ef9de84f04a8ad5efe3e3000ad
joseflekardal <joseflekardal@gmail.com>
2024-12-12 10:35:28 +0100

    feat: more setup

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import { Hono } from "@hono/hono";
+import { Hono } from "hono";
 import { cors } from "hono/cors";
 
 const app = new Hono();
 
 app.use("*", cors());
 
-app.get("/", (c) => c.text("Hello world"));
+app.get("/", (c) => c.text("Hello JOsef"));
 
 export function run() {
   Deno.serve(app.fetch);
