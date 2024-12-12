bf3e19cec0821f37c550bac21667c1f677a6df08
Joakim Larsson <joakim.larsson@grade.com>
2024-12-12 13:15:11 +0100

    added status endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { db } from "./db.ts";
 import * as queue from "./queue.ts";
 
 const app = new Hono();
@@ -9,6 +10,19 @@ app.use("*", cors());
 app.post("/send-notification", (c) => {
   queue.push("waddup");
   return c.text("OK", 200);
+});
+
+app.get("/status/:id", (c) => {
+  const id = c.req.param('id');
+
+  const messages = db.prepare(
+    `
+    select *
+    from messages
+    where id = ?;
+    `
+   ).all(id);
+  return c.json(messages);
 });
 
 export function run() {
